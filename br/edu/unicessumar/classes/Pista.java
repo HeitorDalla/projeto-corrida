3d212e1badc06104ddbedcd7651d128fe2c0ec71
matheusolavo <olavo6013@gmail.com>
2025-08-25 21:39:01 -0300

    atributos e construtores

--- br/edu/unicessumar/classes/Pista.java
+++ br/edu/unicessumar/classes/Pista.java
@@ -1,5 +1,16 @@
 package br.edu.unicessumar.classes;
 
 public class Pista {
+    private String nome;
+    private String distancia;
+    private String tipoPista;
+    private String clima;
+
+    public Pista(String nome, String distancia, String tipoPista, String clima){
+        this.nome = nome;
+        this.distancia = distancia;
+        this.tipoPista = tipoPista;
+        this.clima = clima;
+    }
     
 }
