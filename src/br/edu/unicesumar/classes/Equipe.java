41d34ce0979857ed390a43f8fd7a6d73c3a983c1
HeitorDalla <heitorvillavilla@gmail.com>
2025-08-25 21:52:17 -0300

    Finalizado as classes de jogador e equipe

--- src/br/edu/unicesumar/classes/Equipe.java
+++ src/br/edu/unicesumar/classes/Equipe.java
@@ -1,5 +1,22 @@
 package src.br.edu.unicesumar.classes;
 
 public class Equipe {
-    
+    private int id;
+    private String roupa;
+    private String marca;
+
+    /* Função para retornar o id da equipe */
+    public int getId () {
+        return this.id;
+    }
+
+    /* Função para retornar a roupa da equipe */
+    public String getRoupa () {
+        return this.roupa;
+    }
+
+    /* Função para retornar a marca da equipe */
+    public String getMarca () {
+        return this.marca;
+    }
 }
